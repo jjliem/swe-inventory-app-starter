18060fdbc7c83617a6075044ed10ea8056e8d0f0
Abiye <aabiye7@gmail.com>
2021-07-29 15:15:40 -0500

    icons added, home page created, test

--- models/Brand.test.js
+++ models/Brand.test.js
@@ -11,4 +11,10 @@ describe('Brand Model', () => {
 		expect(testBrand.name).toBe('Magnum')
 	})
 
+	test('can create Brand image ', async () => {
+        await sequelize.sync({ force: true }); 
+        const testBrandimage = await Brand.create({ name: 'Magnum', image: 'https://pbs.twimg.com/profile_images/1154022165949423616/ttYGBBGT_400x400.png' })
+        expect(testBrandimage.id).toBe(1)
+    })
+
 })
